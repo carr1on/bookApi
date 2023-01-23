d8dcac36b5eb5407729046cd5121f7cef3462560
carr1on <salamander001@ya.ru>
2023-01-23 15:58:08 +0300

    remove junk variable strings for try connection to db

--- controller/app/app.go
+++ controller/app/app.go
@@ -1,11 +1,10 @@
 package app
 
 import (
-	//"gorm.io/driver/sqlite"  //1
-	//	"gorm.io/gorm" //1
-	"github.com/jinzhu/gorm"                  //2
-	_ "github.com/jinzhu/gorm/dialects/mysql" //2
-	//_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"github.com/jinzhu/gorm"                  
+	_ "github.com/jinzhu/gorm/dialects/mysql" 
+
 	"log"
 )
 
@@ -14,18 +13,9 @@ var (
 )
 
 func Connect() {
-	/* //1
-	d, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		log.Print("failed to connect database")
-	}*/
 
-	//2
 	d, err := gorm.Open("mysql", "root:mariadb@/tmp?charset=utf8&parseTime=True&loc=Local")
-	// postgreSQL  // _ "github.com/jinzhu/gorm/dialects/postgres"
-	//d, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=postgreSQL")
 
-	//	defer d.Close()
 	if err != nil {
 		log.Print("failed to connect database")
 	}
